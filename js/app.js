a7322a30d061fef316baf190ecd61b840e66c1ce
Gerasimos <gerasimos.gakis@gmail.com>
2017-11-15 15:22:20 +0000

    Test

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ Enemy.prototype.render = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+//sdfgsdgdfhtfgrh
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
